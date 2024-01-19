f038cc997e87267307c072ed47c068c78afa5ca5
Krishnadeol <114430936+Krishnadeol@users.noreply.github.com>
2024-01-19 15:45:11 +0530

    added some more resposive components

--- src/components/Notecard.js
+++ src/components/Notecard.js
@@ -3,10 +3,17 @@ import React from 'react';
 export default function Notecard(props) {
   return (
     <div>
-      <div className="card">
+      <div className="card my-4">
         <div className="card-body">
-          <h5 className="card-title">{props.i.title}</h5>
-          <p className="card-text">{props.i.body}</p>
+          <div className="d-flex align-items-center justify-content-between">
+
+            <h5 className="card-title">{props.i.title}</h5>
+            <div className="d-flex align-items-center">
+              <i className="fa-solid fa-trash mx-2"></i>
+              <i className="fa-solid fa-pen-to-square mx-2"></i>
+            </div>
+          </div>
+          <p className="card-text ml-2">{props.i.body}</p>
         </div>
       </div>
     </div>
